d0a20a33c3019521fc31ffa1dc7ffc2375c2a58c
Frank Doyle <fdoyl001@gmail.com>
2020-01-14 21:51:59 -0600

    fix image loading

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,6 +73,15 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-remark-images`,
+      options: {
+        // It's important to specify the maxWidth (in pixels) of
+        // the content container as this plugin uses this as the
+        // base for generating different widths of each image.
+        maxWidth: 590,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
